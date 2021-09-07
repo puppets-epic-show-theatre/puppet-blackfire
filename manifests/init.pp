1c9f1c725ae36aa773ef3e617f08d9ed3c45db45
Denis Barrachina Claudio <denis.barrachina@check24.de>
2021-09-07 19:29:26 +0200

    Corrected parameter Types

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
 # Installs and configures Blackfire agent and PHP extension
 class blackfire (
-  String $server_id = $blackfire::params::server_id,
-  String $server_token = $blackfire::params::server_token,
+  String[1] $server_id,
+  String[1] $server_token,
   Hash $agent = $blackfire::params::agent,
   Hash $php = $blackfire::params::php,
-  Hash $manage_repo = $blackfire::params::manage_repo
+  Boolean $manage_repo = $blackfire::params::manage_repo
 ) inherits blackfire::params {
 
   anchor { '::blackfire::begin': }
